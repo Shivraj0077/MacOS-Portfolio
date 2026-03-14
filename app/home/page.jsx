9b0ba08d06b37e0081fda30947cd28b142cef605
Shivraj <shivrajpawar6906@gmail.com>
2026-03-14 15:47:28 +0530

    Boot screen with svg logo rendered

---
+++ app/home/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-neutral-900 text-white">
+      <h1 className="text-6xl font-bold">
+        Hello
+      </h1>
+    </div>
+  );
+}
